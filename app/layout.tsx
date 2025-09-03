58f1df09310365b0d620bc998776a68d80bbaa5c
Luiz Guilherme <luizbraga@live.com>
2025-09-03 18:32:56 -0300

    Nome da pagina

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Manual Cesária',
+  description: 'Manual de uso do cesária App',
   generator: 'v0.app',
 }
 
